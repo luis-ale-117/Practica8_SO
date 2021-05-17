a7c7423ef293cd15d99fec147d36ae8e4ab6fd4c
Luis Ale Mendoza <lalexmf117@gmail.com>
2021-05-17 18:00:30 -0500

    Detecta las teclas presionadas, incluyendo Ctrl+Algo

--- Programa81.c
+++ Programa81.c
@@ -1,10 +1,22 @@
 #include<ncurses.h>
 
+#define CTRL(x) (x & 0x1F)
+
 int main(void){	
 	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
+	noecho();
+    char caracterEscrito;
+
+    printw("SUPER EDITOR DE TEXTO OSIOSI...\nPRESIONE CTRL+A PARA SALIR...\n");
+    while (caracterEscrito=getch())
+    {                
+        if (caracterEscrito == CTRL('A'))
+        {            
+            break;
+        }
+        printw("Presionaste: %s, En ASCII: 0x%02x\n",keyname(caracterEscrito),caracterEscrito);
+    }
+    
 	endwin();			/* End curses mode		  */
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,2 @@
-all:Programa81
-
 Programa81:Programa81.c
 	gcc Programa81.c -o Programa81 -lncurses
