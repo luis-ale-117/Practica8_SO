22cec9105e2cb3208b55ff2685fca5596a978fa4
Luis Ale Mendoza <lalexmf117@gmail.com>
2021-05-20 00:46:30 -0500

    Una interfaz un poco mas bonita oshi

--- Programa81.c
+++ Programa81.c
@@ -3,9 +3,16 @@
 
 #define CTRL(x) (x & 0x1F)
 
+void creaVentanaEntrada();
+void creaVentanaPresentacion();
+void creaVentanaArchivo();
+
 void despliegaPresentacion();
 void despliegaInstrucciones();
 void despliegaInfoArchivo(char* info);
+
+
+WINDOW *ventEntrada,*ventPresentacion,*ventArchivo;
 
 int main(void){	
 	initscr();			/* Start curses mode 		  */
@@ -14,32 +21,40 @@ int main(void){
     char textoNuevo[200]="";
     char caracterEscrito;
     
-    despliegaPresentacion();
+    creaVentanaPresentacion();
+    creaVentanaArchivo();
+    creaVentanaEntrada();  
+    
     despliegaInfoArchivo(textoArchivo);
+    wrefresh(ventArchivo);
+    //refresh();  
+
     /*Empieza a detectar las teclas presionadas*/
-    while (caracterEscrito=getch())
+    while (caracterEscrito=wgetch(ventEntrada))
     {                
         if (caracterEscrito == CTRL('A'))        {            
             break;
         }
-        else if(caracterEscrito == CTRL('D')){
-            despliegaInstrucciones();
-            echo();                        
-            getstr(textoNuevo);            
+        else if(caracterEscrito == CTRL('D')){            
+            echo();
+            
+            wmove(ventEntrada,1,1);
+            wgetstr(ventEntrada,textoNuevo);            
             strcat(textoArchivo,textoNuevo);
             strcat(textoArchivo,"\n");
-            clear();
-            despliegaPresentacion();
             despliegaInfoArchivo(textoArchivo);
-            
+            wrefresh(ventArchivo);
+
             noecho();
         }
-        else if (caracterEscrito == CTRL('F'))
+        /*else if (caracterEscrito == CTRL('F'))
         {
             printw("\nGUARDADO UWU .......\n");
-        }
+        }*/
         else{
-            printw("SIN ACCION, PRESIONE CTRL + char\n");
+            //wmove(ventArchivo,1,1);
+            mvwprintw(ventArchivo,1,1,"SIN ACCION, PRESIONE CTRL + char");
+            wrefresh(ventArchivo);
         }        
         
     }        
@@ -48,15 +63,45 @@ int main(void){
 }
 
 
+void creaVentanaEntrada(){
+    int yMax, xMax;
+    getmaxyx(stdscr,yMax,xMax);
+    ventEntrada = newwin(3,xMax-5,yMax-5,3);
+    box(ventEntrada,0,0);
+    wrefresh(ventEntrada);
+    wmove(ventEntrada,1,1);
+}
+void creaVentanaPresentacion(){
+    int yMax, xMax;
+    getmaxyx(stdscr,yMax,xMax);
+    ventPresentacion = newwin(6,xMax-5,1,3);
+    box(ventPresentacion,0,0);
+    wrefresh(ventPresentacion);
+    despliegaPresentacion();    
+}
+void creaVentanaArchivo(){
+    int yMax, xMax;
+    getmaxyx(stdscr,yMax,xMax);
+    ventArchivo = newwin(12,xMax-5,9,3);
+    box(ventArchivo,0,0);
+    wrefresh(ventArchivo);       
+}
+
 
 void despliegaPresentacion(){
     /*Presentacion y explicacion de comandos de control*/
-    printw("SUPER EDITOR DE TEXTO OSIOSI...\nPRESIONE CTRL+A PARA SALIR...\nPRESIONE CTRL+D PARA ESCRIBIR NUEVA LINEA AL FINAL...\n\n");    
+    wmove(ventPresentacion,1,1);
+    wprintw(ventPresentacion,"SUPER EDITOR DE TEXTO OSIOSI...");
+    wmove(ventPresentacion,2,1);
+    wprintw(ventPresentacion,"PRESIONE CTRL+A PARA SALIR...");
+    wmove(ventPresentacion,3,1);
+    wprintw(ventPresentacion,"PRESIONE CTRL+D PARA ESCRIBIR NUEVA LINEA AL FINAL...");    
+    wrefresh(ventPresentacion);
 }
 void despliegaInstrucciones(){
     /*Presentacion y explicacion de comandos de control*/
     printw("INSERTA TEXTO,PRESIONA ENTER PARA ANADIR:\n");
 }
 void despliegaInfoArchivo(char* info){
-    printw(info);
+    mvwprintw(ventArchivo,1,1,info);
 }
